6910ecb2b5ade4d53ff5938c1bf3ad1a42e0def9
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2024-10-14 20:01:20 +0100

    Fix fs not found error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Fix for Module not found: Can't resolve fs.
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
